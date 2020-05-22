c4e8b74bde85177e057f7fcd53096fe0834c74f4
Minal Dhansukhlal <dhansukhlalm@student.douglascollege.ca>
2020-05-22 00:40:35 -0700

    Method output created

--- MinalProject/src/myAppTester.java
+++ MinalProject/src/myAppTester.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -19,6 +21,19 @@ public class myAppTester {
 		double salary;
 		salary=hourlyPay*noOfHours;
 		return salary;
+	}
+	public static void output() {
+		Scanner input=new Scanner(System.in);
+		System.out.println("Hello Software Engineering World");
+
+		System.out.println("Hi My name is Minal");
+
+		System.out.println("Enter Hourly Pay: ");
+		double hourPay=input.nextDouble();
+		System.out.println("Enter number of hours worked: ");
+		double hours=input.nextDouble();
+		System.out.println(monthlyPay(hourPay, hours) + "dollars");
+		System.out.println("Have a good day");
 	}
 
 }
